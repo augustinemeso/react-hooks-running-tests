83149980c2790f3fde1e9b7984a80c3f417071a7
augustinemeso <augustinemesobk@gmail.com>
2024-11-04 21:31:09 +0300

    add REACT-HOOKS-TESTS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function Header() {
+  return <h1>hello from the Header!</h1>;
+}
+
+export default Header;
